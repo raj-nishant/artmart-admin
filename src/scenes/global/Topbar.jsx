b248cff4d99ec2493a75688e2020444f320dfe43
Nishant Raj <79581018+raj-nishant@users.noreply.github.com>
2024-04-10 15:30:49 +0530

    commit

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,4 +1,6 @@
-import { Box, IconButton, useTheme } from "@mui/material";
+import React, { useState } from "react";
+
+import { Box, IconButton, useTheme, Menu, MenuItem } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
 import InputBase from "@mui/material/InputBase";
@@ -8,8 +10,29 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
+import { useAuth } from "../../services/AuthContext";
 
 const Topbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const {
+    user,
+    login,
+    logout,
+    isAuthenticated,
+    userDetails,
+    fetchUserDetails,
+  } = useAuth();
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
@@ -43,9 +66,32 @@ const Topbar = () => {
         <IconButton>
           <SettingsOutlinedIcon />
         </IconButton>
-        <IconButton>
+        <IconButton onClick={handleMenuClick}>
           <PersonOutlinedIcon />
         </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+          PaperProps={{
+            sx: {
+              minWidth: "200px", // Adjust the minimum width of the menu
+            },
+          }}
+        >
+          <MenuItem onClick={handleMenuClose} sx={{ fontSize: "1rem" }}>
+            Name
+          </MenuItem>
+          <MenuItem onClick={handleMenuClose} sx={{ fontSize: "1rem" }}>
+            Profile
+          </MenuItem>
+          <MenuItem onClick={handleMenuClose} sx={{ fontSize: "1rem" }}>
+            Settings
+          </MenuItem>
+          <MenuItem onClick={handleLogout} sx={{ fontSize: "1rem" }}>
+            Logout
+          </MenuItem>
+        </Menu>
       </Box>
     </Box>
   );
