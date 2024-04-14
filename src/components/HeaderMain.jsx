5353a5c5df0d8557fad99ac5678cf6fada19d785
Nishant Raj <79581018+raj-nishant@users.noreply.github.com>
2024-04-14 15:07:47 +0530

    commit

--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../services/AuthContext";
-import { Avatar, Modal, TextField, Button } from "@mui/material";
+import {
+  Avatar,
+  Modal,
+  TextField,
+  Button,
+  IconButton,
+  Drawer,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import ScrollingBanner from "../scenes/register/ScrollingBanner";
 
 const HeaderMain = () => {
-  const {
-    user,
-    login,
-    logout,
-    isAuthenticated,
-    userDetails,
-    fetchUserDetails,
-  } = useAuth();
-
-  const [showUserDetails, setShowUserDetails] = useState(false);
+  const { isAuthenticated, userDetails, fetchUserDetails } = useAuth();
   const [showContactModal, setShowContactModal] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [query, setQuery] = useState("");
@@ -23,30 +23,13 @@ const HeaderMain = () => {
 
   useEffect(() => {
     if (isAuthenticated() && !userDetails) {
-      fetchUserDetails(); // Fetch user details only if authenticated and userDetails is not already fetched
+      fetchUserDetails();
     }
   }, [isAuthenticated()]);
 
-  const handleLogout = () => {
-    logout();
-  };
-
-  const handleLogoutClick = () => {
-    handleLogout();
-    setShowUserDetails(false); // Close the tooltip after logout
-    navigate("/login");
-  };
-
-  const handleTooltipMouseEnter = () => {
-    setShowUserDetails(true);
-  };
-
-  const handleTooltipMouseLeave = () => {
-    setShowUserDetails(false);
-  };
-
   const handleContactButtonClick = () => {
     setShowContactModal(true);
+    toggleMenu(); // Close menu when opening the contact modal
   };
 
   const handleCloseModal = () => {
@@ -54,79 +37,70 @@ const HeaderMain = () => {
   };
 
   const handleSubmit = () => {
-    // Implement your logic to handle form submission
     console.log("Name:", name);
     console.log("Email:", email);
     console.log("Query:", query);
-    // Reset fields and close modal
     setName("");
     setEmail("");
     setQuery("");
     setShowContactModal(false);
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <ScrollingBanner />
-      <header className=" text-black w-full px-7 py-7  flex items-center justify-between bg-gradient-to-b from-slate-200 to-white">
+      <header className="text-black w-full px-7 py-7 flex items-center justify-between bg-gradient-to-b from-slate-200 to-white">
+        <div className="md:hidden">
+          <IconButton onClick={toggleMenu}>
+            <MenuIcon />
+          </IconButton>
+        </div>
         <Link to={"/"}>
           <img className="w-32" src="/logo.png" alt="Logo" />
         </Link>
-        <div className="flex items-center">
-          {isAuthenticated() && userDetails ? (
-            <div
-              className="relative"
-              onMouseEnter={handleTooltipMouseEnter}
-              onMouseLeave={handleTooltipMouseLeave}
-            >
-              <Avatar
-                alt={userDetails.name}
-                src={userDetails.profilePhotoUrl}
-              />
-              {showUserDetails && userDetails && (
-                <div className="absolute flex flex-col items-center bg-white shadow-md rounded-md p-5 w-40 gap-2 right-0 top-full">
-                  <div>
-                    Hi,
-                    <span className="text-yellow-600"> {userDetails.name}</span>
-                  </div>
-                  {/* <div>{userDetails.email}</div> */}
-
-                  <Link to={"/profile"}>
-                    <button className="text-gray-700 p-2  underline hover:text-gray-900">
-                      My Account
-                    </button>
-                  </Link>
-
-                  <button
-                    onClick={handleLogoutClick}
-                    className="text-red-500 p-2 underline hover:text-red-700"
-                  >
-                    Logout
-                  </button>
-                </div>
-              )}
-            </div>
-          ) : (
-            <>
-              <Link to={"/login"}>
-                <button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded mr-4">
-                  Login
-                </button>
-              </Link>
-              <Link to={"/register"}>
-                <button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded mr-4">
-                  Join Us
-                </button>
-              </Link>
-              <button
-                onClick={handleContactButtonClick}
-                className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded mr-4"
-              >
-                Contact Us
-              </button>
-            </>
-          )}
+        <div className="hidden md:flex items-center">
+          <Link to={"/login"}>
+            <button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded mr-4">
+              Login
+            </button>
+          </Link>
+          <Link to={"/register"}>
+            <button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded mr-4">
+              Join Us
+            </button>
+          </Link>
+          <button
+            onClick={handleContactButtonClick}
+            className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
+          >
+            Contact Us
+          </button>
         </div>
+        <Drawer anchor="left" open={menuOpen} onClose={toggleMenu}>
+          <div className="p-5">
+            <Link to={"/login"} className="block mb-2">
+              <Button fullWidth variant="outlined" onClick={toggleMenu}>
+                Login
+              </Button>
+            </Link>
+            <Link to={"/register"} className="block mb-2">
+              <Button fullWidth variant="outlined" onClick={toggleMenu}>
+                Join Us
+              </Button>
+            </Link>
+            <Button
+              fullWidth
+              variant="outlined"
+              onClick={handleContactButtonClick}
+            >
+              Contact Us
+            </Button>
+          </div>
+        </Drawer>
         <Modal open={showContactModal} onClose={handleCloseModal}>
           <div className="bg-white p-8 rounded-md w-80 mx-auto mt-20">
             <h2 className="text-2xl mb-4">Contact Us</h2>
